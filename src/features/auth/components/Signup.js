5b7de6c05450a0d14540a6dbeaf735822271a26a
vaishnav27 <0808ci191046.ies@ipsacademy.org>
2024-04-15 22:49:26 +0530

    signup issue resolved

--- src/features/auth/components/Signup.js
+++ src/features/auth/components/Signup.js
@@ -16,7 +16,7 @@ export default function Signup() {
 
   return (
     <>
-      {user?.email}
+      {user && <Navigate to="/" replace={true}></Navigate>}
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
